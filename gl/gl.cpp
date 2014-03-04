72b9c1bed5bad33fdd4230e3e0f85bff3aa604dd
Matheus Ribeiro <mfribeiro@gmail.com>
2014-03-03 23:32:53 -0300

    funcaao de retangulo em gl

--- gl/gl.cpp
+++ gl/gl.cpp
@@ -2,6 +2,23 @@
 
 namespace gl {
 
+#if USAR_OPENGL_ES
+inline void Retangulo(GLfloat x1, GLFloat y1, GLfloat x2, GLfloat y2) {
+  const unsigned short indices[] = { 0, 1, 2, 3, 4, 5, 6, 7, };
+  const float vertices[] = {
+    x1, y1,
+    x2, y1,
+    x2, y2,
+    x1, y2,
+  };
+  gl::HabilitaEstadoCliente(GL_VERTEX_ARRAY);
+  gl::PonteiroVertices(2, GL_FLOAT, vertices);
+  gl::DesenhaElementos(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_SHORT, indices);
+  gl::DesabilitaEstadoCliente(GL_VERTEX_ARRAY);
+
+}
+#endif
+
 void ConeSolido(GLfloat base, GLfloat altura, GLint num_fatias, GLint num_tocos) {
 #if USAR_OPENGL_ES
 #else
