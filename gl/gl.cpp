bde8436a517021d83739c481b354b76585702538
Matheus Ribeiro <mfribeiro@gmail.com>
2014-03-06 02:12:56 -0300

    comecei a fazer a desprojecao manual mas ainda esta imprecisa

--- gl/gl.cpp
+++ gl/gl.cpp
@@ -110,6 +110,91 @@ void MultiplicaMatrizVetor(const float m[16], float vetor[4]) {
   vetor[3] = res[3];
 }
 
+/*
+** Invert 4x4 matrix.
+** Contributed by David Moore (See Mesa bug #6748)
+*/
+static int __gluInvertMatrixf(const GLfloat m[16], GLfloat invOut[16])
+{
+    GLfloat inv[16], det;
+    int i;
+
+    inv[0] =   m[5]*m[10]*m[15] - m[5]*m[11]*m[14] - m[9]*m[6]*m[15]
+             + m[9]*m[7]*m[14] + m[13]*m[6]*m[11] - m[13]*m[7]*m[10];
+    inv[4] =  -m[4]*m[10]*m[15] + m[4]*m[11]*m[14] + m[8]*m[6]*m[15]
+             - m[8]*m[7]*m[14] - m[12]*m[6]*m[11] + m[12]*m[7]*m[10];
+    inv[8] =   m[4]*m[9]*m[15] - m[4]*m[11]*m[13] - m[8]*m[5]*m[15]
+             + m[8]*m[7]*m[13] + m[12]*m[5]*m[11] - m[12]*m[7]*m[9];
+    inv[12] = -m[4]*m[9]*m[14] + m[4]*m[10]*m[13] + m[8]*m[5]*m[14]
+             - m[8]*m[6]*m[13] - m[12]*m[5]*m[10] + m[12]*m[6]*m[9];
+    inv[1] =  -m[1]*m[10]*m[15] + m[1]*m[11]*m[14] + m[9]*m[2]*m[15]
+             - m[9]*m[3]*m[14] - m[13]*m[2]*m[11] + m[13]*m[3]*m[10];
+    inv[5] =   m[0]*m[10]*m[15] - m[0]*m[11]*m[14] - m[8]*m[2]*m[15]
+             + m[8]*m[3]*m[14] + m[12]*m[2]*m[11] - m[12]*m[3]*m[10];
+    inv[9] =  -m[0]*m[9]*m[15] + m[0]*m[11]*m[13] + m[8]*m[1]*m[15]
+             - m[8]*m[3]*m[13] - m[12]*m[1]*m[11] + m[12]*m[3]*m[9];
+    inv[13] =  m[0]*m[9]*m[14] - m[0]*m[10]*m[13] - m[8]*m[1]*m[14]
+             + m[8]*m[2]*m[13] + m[12]*m[1]*m[10] - m[12]*m[2]*m[9];
+    inv[2] =   m[1]*m[6]*m[15] - m[1]*m[7]*m[14] - m[5]*m[2]*m[15]
+             + m[5]*m[3]*m[14] + m[13]*m[2]*m[7] - m[13]*m[3]*m[6];
+    inv[6] =  -m[0]*m[6]*m[15] + m[0]*m[7]*m[14] + m[4]*m[2]*m[15]
+             - m[4]*m[3]*m[14] - m[12]*m[2]*m[7] + m[12]*m[3]*m[6];
+    inv[10] =  m[0]*m[5]*m[15] - m[0]*m[7]*m[13] - m[4]*m[1]*m[15]
+             + m[4]*m[3]*m[13] + m[12]*m[1]*m[7] - m[12]*m[3]*m[5];
+    inv[14] = -m[0]*m[5]*m[14] + m[0]*m[6]*m[13] + m[4]*m[1]*m[14]
+             - m[4]*m[2]*m[13] - m[12]*m[1]*m[6] + m[12]*m[2]*m[5];
+    inv[3] =  -m[1]*m[6]*m[11] + m[1]*m[7]*m[10] + m[5]*m[2]*m[11]
+             - m[5]*m[3]*m[10] - m[9]*m[2]*m[7] + m[9]*m[3]*m[6];
+    inv[7] =   m[0]*m[6]*m[11] - m[0]*m[7]*m[10] - m[4]*m[2]*m[11]
+             + m[4]*m[3]*m[10] + m[8]*m[2]*m[7] - m[8]*m[3]*m[6];
+    inv[11] = -m[0]*m[5]*m[11] + m[0]*m[7]*m[9] + m[4]*m[1]*m[11]
+             - m[4]*m[3]*m[9] - m[8]*m[1]*m[7] + m[8]*m[3]*m[5];
+    inv[15] =  m[0]*m[5]*m[10] - m[0]*m[6]*m[9] - m[4]*m[1]*m[10]
+             + m[4]*m[2]*m[9] + m[8]*m[1]*m[6] - m[8]*m[2]*m[5];
+
+    det = m[0]*inv[0] + m[1]*inv[4] + m[2]*inv[8] + m[3]*inv[12];
+    if (det == 0)
+        return GL_FALSE;
+
+    det=1.0f/det;
+
+    for (i = 0; i < 16; i++)
+        invOut[i] = inv[i] * det;
+
+    return GL_TRUE;
+}
+
+static void __gluMultMatrixVecf(const GLfloat matrix[16], const GLfloat in[4],
+                                GLfloat out[4])
+{
+    int i;
+
+    for (i=0; i<4; i++)
+    {
+        out[i] = in[0] * matrix[0*4+i] +
+                 in[1] * matrix[1*4+i] +
+                 in[2] * matrix[2*4+i] +
+                 in[3] * matrix[3*4+i];
+    }
+}
+
+static void __gluMultMatricesf(const GLfloat a[16], const GLfloat b[16],
+                               GLfloat r[16])
+{
+    int i, j;
+
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            r[i*4+j] = a[i*4+0]*b[0*4+j] +
+                       a[i*4+1]*b[1*4+j] +
+                       a[i*4+2]*b[2*4+j] +
+                       a[i*4+3]*b[3*4+j];
+        }
+    }
+}
+
 struct ContextoInterno {
   // Mapeia um ID para a cor RGB em 22 bits (os dois mais significativos sao para a pilha).
   std::unordered_map<unsigned int, unsigned int> ids;
@@ -363,11 +448,49 @@ void OlharPara(GLfloat eyex, GLfloat eyey, GLfloat eyez, GLfloat centerx,
   glTranslatef(-eyex, -eyey, -eyez);
 }
 
-GLint Desprojeta(float x_janela, float y_janela, float profundidade_3d,
-                 const float* model, const float* proj, const GLint* view,
-                 float* x3d, float* y3d, float* z3d) {
-  // TODO
-  return 0;
+GLint Desprojeta(GLfloat winx, GLfloat winy, GLfloat winz,
+                 const GLfloat modelMatrix[16],
+                 const GLfloat projMatrix[16],
+                 const GLint viewport[4],
+                 GLfloat* objx, GLfloat* objy, GLfloat* objz) {
+  GLfloat finalMatrix[16];
+  GLfloat in[4];
+  GLfloat out[4];
+
+  __gluMultMatricesf(modelMatrix, projMatrix, finalMatrix);
+  if (!__gluInvertMatrixf(finalMatrix, finalMatrix))
+  {
+      return(GL_FALSE);
+  }
+
+  in[0]=winx;
+  in[1]=winy;
+  in[2]=winz;
+  in[3]=1.0;
+
+  /* Map x and y from window coordinates */
+  in[0] = (in[0] - viewport[0]) / viewport[2];
+  in[1] = (in[1] - viewport[1]) / viewport[3];
+
+  /* Map to range -1 to 1 */
+  in[0] = in[0] * 2 - 1;
+  in[1] = in[1] * 2 - 1;
+  in[2] = in[2] * 2 - 1;
+
+  __gluMultMatrixVecf(finalMatrix, in, out);
+  if (out[3] == 0.0)
+  {
+      return(GL_FALSE);
+  }
+
+  out[0] /= out[3];
+  out[1] /= out[3];
+  out[2] /= out[3];
+  *objx = out[0];
+  *objy = out[1];
+  *objz = out[2];
+
+  return(GL_TRUE);
 }
 
 void MatrizPicking(float x, float y, float delta_x, float delta_y, GLint *viewport) {
@@ -398,12 +521,12 @@ GLint ModoRenderizacao(modo_renderizacao_e modo) {
       // Usar void* para imprimir em hexa.
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wint-to-pointer-cast"
-      LOG(INFO) << "Pixel: " << (void*)pixel[0] << " " << (void*)pixel[1] << " " << (void*)pixel[2] << " " << (void*)pixel[3];;
+      VLOG(2) << "Pixel: " << (void*)pixel[0] << " " << (void*)pixel[1] << " " << (void*)pixel[2] << " " << (void*)pixel[3];;
       unsigned int id_mapeado = pixel[0] | (pixel[1] << 8) | (pixel[2] << 16);
-      LOG(INFO) << "Id mapeado: " << (void*)id_mapeado;
+      VLOG(2) << "Id mapeado: " << (void*)id_mapeado;
 #pragma GCC diagnostic push
       unsigned int pos_pilha = id_mapeado >> 22;
-      LOG(INFO) << "Pos pilha: " << pos_pilha;
+      VLOG(2) << "Pos pilha: " << pos_pilha;
       if (pos_pilha == 0 || pos_pilha > 3) {
         LOG(ERROR) << "Pos pilha invalido: " << pos_pilha;
         return 0;
@@ -414,7 +537,7 @@ GLint ModoRenderizacao(modo_renderizacao_e modo) {
         return 0;
       }
       unsigned int id_original = it->second;
-      LOG(INFO) << "Id original: " << id_original;
+      VLOG(2) << "Id original: " << id_original;
       GLuint* ptr = g_contexto.buffer_selecao;
       ptr[0] = pos_pilha;
       ptr[1] = 0.0f;  // zmin.
