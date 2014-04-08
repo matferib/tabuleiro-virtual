79f359fc1157c25069c0e4f3e8427c04fe8b6e95
Matheus Ribeiro <mfribeiro@gmail.com>
2014-04-08 00:20:22 -0300

    erro de compilacao no android

---
+++ android/src/com/matferib/Tabuleiro/SelecaoActivity.java
@@ -0,0 +1,27 @@
+package com.matferib.Tabuleiro;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.app.Activity;
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class SelecaoActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+  }
+}
