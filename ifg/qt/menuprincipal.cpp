862f6ff2b49ca3e1f4cb00b4a4b58b510b23660e
Matheus Ribeiro <mfribeiro@gmail.com>
2014-06-16 23:49:21 -0300

    atualizacao de versao

--- ifg/qt/menuprincipal.cpp
+++ ifg/qt/menuprincipal.cpp
@@ -345,7 +345,7 @@ void MenuPrincipal::TrataAcaoItem(QAction* acao){
     QMessageBox::about(
         qobject_cast<QWidget*>(parent()),
         tr("Sobre o tabuleiro virtual"),
-        tr("Tabuleiro virtual versão 1.4.4\n"
+        tr("Tabuleiro virtual versão 1.4.5\n"
            "Bibliotecas: QT, OpenGL, Protobuf, Boost\n"
            "Autor: Matheus Ribeiro <mfribeiro@gmail.com>"));
   }
