498330aac28ce22d073794bf1cb5dfb5e83b1c5f
Matheus Ribeiro <mfribeiro+planilha@gmail.com>
2015-04-16 00:07:49 -0300

    porta padrao de volta 11223

--- net/util.h
+++ net/util.h
@@ -12,10 +12,12 @@ const std::string CodificaDados(const std::string& dados);
 unsigned int DecodificaTamanho(const std::string::iterator& string);
 
 // Porta para aceitar conexoes http.
-inline int PortaPadrao() { return 22113; }
+//inline int PortaPadrao() { return 22113; }
+inline int PortaPadrao() { return 11223; }
 
 // Porta de anuncio (broadcast) do endereco do servidor.
-inline int PortaAnuncio() { return 22114; }
+//inline int PortaAnuncio() { return 22114; }
+inline int PortaAnuncio() { return 11224; }
 
 
 // Conversao de numero para string independente de plataforma.
