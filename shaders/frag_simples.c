7617e3533a445ace662724446525f63eb2341138
Matheus Ribeiro <mfribeiro+planilha@gmail.com>
2015-09-11 11:42:07 -0300

    adicionei os shaders simples

---
+++ shaders/frag_simples.c
@@ -0,0 +1,28 @@
+//#version 120
+
+#if defined(GL_ES)
+//precision highp float;
+//#define lowp highp
+//#define mediump highp
+#else
+#define lowp
+#define highp
+#define mediump
+#endif
+
+// Varying sao interpoladas da saida do vertex.
+varying lowp vec4 v_Color;
+varying lowp vec2 v_Tex;  // coordenada texel.
+uniform lowp vec4 gltab_luz_ambiente;      // Cor da luz ambiente.
+
+// Uniforms sao constantes durante desenho, setadas no codigo nativo.
+uniform lowp float gltab_textura;               // Textura ligada? 1.0 : 0.0
+uniform lowp sampler2D gltab_unidade_textura;   // handler da textura.
+
+void main() {
+  lowp vec4 cor_final = v_Color;
+  if (gltab_textura) {
+    cor_final *= texture2D(gltab_unidade_textura, v_Tex.st);
+  }
+  gl_FragColor = cor_final;
+}
