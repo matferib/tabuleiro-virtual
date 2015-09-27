6c1724b2a892ea919d233eea004cc5c7b3cebc89
Matheus Ribeiro <mfribeiro+planilha@gmail.com>
2015-09-27 01:05:54 -0300

    versao do application para 4.2 e gnu_static

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -2,7 +2,8 @@
 #APP_MODULES := tabuleiro protobuf-prebuilt boost-prebuilt
 APP_CPPFLAGS := -std=c++11
 APP_ABI := armeabi-v7a
-APP_PLATFORM := android-19  # 4.4 
-APP_STL := gnustl_shared
+APP_PLATFORM := android-17  # 4.2
+#APP_STL := gnustl_shared
+APP_STL := gnustl_static
 NDK_TOOLCHAIN_VERSION := 4.9
 APP_OPTIM := release
