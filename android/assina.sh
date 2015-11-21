40a6aeee31fb877c75c50965e06b28d8fafacf56
Matheus Ribeiro <mfribeiro+planilha@gmail.com>
2015-11-21 20:00:36 -0200

    pequenas atualizacoes de ios e android

---
+++ android/assina.sh
@@ -0,0 +1,22 @@
+sh compila.sh --release
+
+VERSAO=""
+while [[ $# > 0 ]]; do
+  key="$1"
+  case ${key} in
+    -v|--versao)
+      VERSAO="${2}"
+      shift
+      ;;
+    *)
+    ;;
+  esac
+  shift
+done
+
+test -z "$VERSAO" && echo "Preciso da versao" &&  exit 0 || echo "Assinando versao: ${VERSAO}"
+
+rm -f bin/TabuleiroVirtual-release-${VERSAO}.apk && \
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore tabuleiro-virtual.keystore bin/TabuleiroVirtual-release-unsigned.apk tabvirt && \
+/Users/mribeiro/android/adt/sdk/build-tools/22.0.1/zipalign -v 4 bin/TabuleiroVirtual-release-unsigned.apk bin/TabuleiroVirtual-release-${VERSAO}.apk || \
+echo "Erro assinando"
--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -5,4 +5,4 @@ APP_ABI := armeabi-v7a
 APP_PLATFORM := android-17  # 4.2
 APP_STL := gnustl_shared
 NDK_TOOLCHAIN_VERSION := 4.9
-APP_OPTIM := debug 
+APP_OPTIM := release 
