b6afa7704c4af27fbe276011cfa393ee8f5b3e37
Matheus Ribeiro <mfribeiro+tabuleirolinux@gmail.com>
2016-01-01 22:10:01 -0200

    arquivos faltantes para mac

---
+++ ifg/interface.cpp
@@ -0,0 +1,106 @@
+#include <algorithm>
+#include <functional>
+
+#include "arq/arquivo.h"
+#include "ent/tabuleiro.h"
+#include "ifg/interface.h"
+#include "ifg/tecladomouse.h"
+#include "log/log.h"
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+
+
+namespace ifg {
+
+bool InterfaceGrafica::TrataNotificacao(const ntf::Notificacao& notificacao) {
+  switch (notificacao.tipo()) {
+    case ntf::TN_ABRIR_DIALOGO_ABRIR_TABULEIRO: {
+      TrataAbrirTabuleiro(notificacao);
+      return true;
+    }
+    case ntf::TN_ABRIR_DIALOGO_SALVAR_TABULEIRO: {
+      TrataSalvarTabuleiro(notificacao);
+      return true;
+    }
+    case ntf::TN_INFO:
+    case ntf::TN_ERRO: {
+      TrataMostraMensagem(notificacao.tipo() == ntf::TN_ERRO, notificacao.erro());
+      break;
+    }
+    default:
+      break;
+  }
+  return false;
+}
+
+void InterfaceGrafica::FechaElemento() {
+  teclado_mouse_->MudaEstado(TratadorTecladoMouse::ESTADO_TEMPORIZANDO_MOUSE);
+}
+
+//----------------
+// Mostra Mensagem
+//----------------
+void InterfaceGrafica::TrataMostraMensagem(bool erro, const std::string& mensagem) {
+  tabuleiro_->DesativaWatchdog();
+  MostraMensagem(erro, mensagem, [this] () { tabuleiro_->ReativaWatchdog(); });
+}
+
+//----------------
+// Abrir Tabuleiro
+//----------------
+void InterfaceGrafica::TrataAbrirTabuleiro(const ntf::Notificacao& notificacao) {
+  tabuleiro_->DesativaWatchdog();
+  std::vector<std::string> tab_estaticos =
+      arq::ConteudoDiretorio(arq::TIPO_TABULEIRO_ESTATICO);
+  std::vector<std::string> tab_dinamicos =
+      arq::ConteudoDiretorio(arq::TIPO_TABULEIRO);
+  if (tab_estaticos.size() + tab_dinamicos.size() == 0) {
+    auto* ne = ntf::NovaNotificacao(ntf::TN_ERRO);
+    ne->set_erro(std::string("Nao existem tabuleiros salvos"));
+    central_->AdicionaNotificacao(ne);
+    tabuleiro_->ReativaWatchdog();
+    return;
+  }
+  std::sort(tab_estaticos.begin(), tab_estaticos.end());
+  std::sort(tab_dinamicos.begin(), tab_dinamicos.end());
+  EscolheArquivoAbrirTabuleiro(
+      tab_estaticos, tab_dinamicos,
+      std::bind(
+          &ifg::InterfaceGrafica::VoltaAbrirTabuleiro,
+          this,
+          notificacao.tabuleiro().manter_entidades(),
+          _1, _2));
+}
+
+void InterfaceGrafica::VoltaAbrirTabuleiro(
+    bool manter_entidades, const std::string& nome, arq::tipo_e tipo) {
+  auto* notificacao = ntf::NovaNotificacao(ntf::TN_DESERIALIZAR_TABULEIRO);
+  notificacao->set_endereco(
+      std::string(tipo == arq::TIPO_TABULEIRO_ESTATICO ? "estatico://" : "dinamico://") + nome);
+  notificacao->mutable_tabuleiro()->set_manter_entidades(manter_entidades);
+  central_->AdicionaNotificacao(notificacao);
+  tabuleiro_->ReativaWatchdog();
+}
+
+//-----------------
+// Salvar Tabuleiro
+//-----------------
+void InterfaceGrafica::TrataSalvarTabuleiro(const ntf::Notificacao& notificacao) {
+  tabuleiro_->DesativaWatchdog();
+  EscolheArquivoSalvarTabuleiro(
+      std::bind(
+          &ifg::InterfaceGrafica::VoltaSalvarTabuleiro,
+          this, _1));
+}
+
+void InterfaceGrafica::VoltaSalvarTabuleiro(
+    const std::string& nome) {
+  auto* n = ntf::NovaNotificacao(ntf::TN_SERIALIZAR_TABULEIRO);
+  n->set_endereco(nome);
+  central_->AdicionaNotificacao(n);
+  tabuleiro_->ReativaWatchdog();
+}
+
+}  // namespace ifg
---
+++ ifg/interface.h
@@ -0,0 +1,64 @@
+#ifndef IFG_INTERFACE_H
+#define IFG_INTERFACE_H
+
+#include "arq/arquivo.h"
+#include "ntf/notificacao.h"
+
+namespace ent {
+class Tabuleiro;
+}  // namespace ent
+namespace ifg {
+
+class TratadorTecladoMouse;
+
+class InterfaceGrafica : public ntf::Receptor {
+ public:
+  InterfaceGrafica(
+      TratadorTecladoMouse* teclado_mouse, ent::Tabuleiro* tabuleiro, ntf::CentralNotificacoes* central)
+      : teclado_mouse_(teclado_mouse), tabuleiro_(tabuleiro), central_(central) {
+    central_->RegistraReceptor(this);
+  }
+
+  virtual ~InterfaceGrafica() {
+    central_->DesregistraReceptor(this);
+  }
+
+  bool TrataNotificacao(const ntf::Notificacao& notificacao) override;
+
+ protected:
+  // Mostra um dialogo de erro ou informacao.
+  virtual void MostraMensagem(bool erro, const std::string& mensagem, std::function<void()> funcao_volta) = 0;
+
+  // Mostra dialogo para escolher um item entre tab_estaticos e tab_dinamicos, chamando
+  // a funcao de volta ao terminar.
+  virtual void EscolheArquivoAbrirTabuleiro(
+      const std::vector<std::string>& tab_estaticos,
+      const std::vector<std::string>& tab_dinamicos,
+      std::function<void(const std::string& nome, arq::tipo_e tipo)> funcao_volta) = 0;
+
+  // Mostra dialogo para salvar tabuleiro, chamando a funcao de volta ao terminar.
+  virtual void EscolheArquivoSalvarTabuleiro(
+      std::function<void(const std::string& nome)> funcao_volta) = 0;
+
+  // Fecha elemento e volta teclado e mouse para estado normal.
+  virtual void FechaElemento();
+
+ protected:
+  TratadorTecladoMouse* teclado_mouse_ = nullptr;
+  ent::Tabuleiro* tabuleiro_ = nullptr;
+  ntf::CentralNotificacoes* central_ = nullptr;
+
+ private:
+  void TrataMostraMensagem(bool erro, const std::string& mensagem);
+
+  void TrataAbrirTabuleiro(const ntf::Notificacao& notificacao);
+  void VoltaAbrirTabuleiro(
+    bool manter_entidades, const std::string& nome, arq::tipo_e tipo);
+
+  void TrataSalvarTabuleiro(const ntf::Notificacao& notificacao);
+  void VoltaSalvarTabuleiro(const std::string& nome);
+};
+
+}  // namespace ifg
+
+#endif
