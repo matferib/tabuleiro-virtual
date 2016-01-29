ac2a999d6d7d304d1c650510433fc057ccea79ba
Matheus Ribeiro <mfribeiro+tabuleirolinux@gmail.com>
2016-01-29 02:43:18 -0200

    removi ifdef

--- shaders/frag_profundidade.c
+++ shaders/frag_profundidade.c
@@ -10,8 +10,6 @@
 #define mediump
 #endif
 
-#define BITS_PROFUNDIDADE 16
-
 // Varying sao interpoladas da saida do vertex.
 varying lowp vec4 v_Color;
 varying lowp vec2 v_Tex;  // coordenada texel.
@@ -24,11 +22,7 @@ uniform lowp sampler2D gltab_unidade_textura;   // handler da textura.
 void main() {
   lowp vec4 cor_final = v_Color;
   cor_final *= mix(vec4(1.0), texture2D(gltab_unidade_textura, v_Tex.st), gltab_textura);
-#if BITS_PROFUNDIDADE == 8
-  gl_FragColor = vec4(cor_final.rgb, gl_FragCoord.z);
-#else
   highp vec4 v4 = vec4(cor_final.rg, gl_FragCoord.z, fract(gl_FragCoord.z * 256.0));
   v4.b -= v4.a / 256.0;  // Tira a ultima parte, melhora e muito a precisao.
   gl_FragColor = v4;
-#endif
 }
