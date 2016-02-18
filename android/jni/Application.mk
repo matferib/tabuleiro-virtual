fbb9644502178a4d856aa8e597bc81094658654a
Matheus Ribeiro <mfribeiro+tabuleirolinux@gmail.com>
2016-02-18 02:03:52 -0200

    debug no application.mk nao sei se funciona

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -4,5 +4,10 @@ APP_CPPFLAGS := -std=c++11
 APP_ABI := armeabi-v7a
 APP_PLATFORM := android-17  # 4.2
 APP_STL := gnustl_shared
-NDK_TOOLCHAIN_VERSION := 4.9
+#NDK_TOOLCHAIN_VERSION := 4.9
+NDK_TOOLCHAIN_VERSION := clang
+ifeq ($(DEBUG),"1")
 APP_OPTIM := debug
+else
+APP_OPTIM := release
+endif
