d81f4a3a9a050acfe0be1e3801310843ed8da740
Matheus Ribeiro <mfribeiro+tabuleirolinux@gmail.com>
2016-10-16 14:39:20 -0200

    correcao na extracao de modelos, nao estava aplicando escala

--- m3d/m3d.cpp
+++ m3d/m3d.cpp
@@ -191,7 +191,9 @@ void Modelos3d::CarregaModelo3d(const std::string& id_interno) {
   n.mutable_tabuleiro()->mutable_entidade(0)->mutable_pos()->clear_y();
   VLOG(1) << "Carregando modelo 3d " << id_interno << " (" << nome_arquivo << ")";
   VLOG(2) << n.DebugString();
-  interno_->modelos[id_interno].vbos_nao_gravados = ent::Entidade::ExtraiVbo(n.tabuleiro().entidade(0), &ent::ParametrosDesenho::default_instance());
+  // Extrai com mundo, por causa da escala. A posicao XY ja foi limpa.
+  interno_->modelos[id_interno].vbos_nao_gravados =
+      ent::Entidade::ExtraiVbo(n.tabuleiro().entidade(0), &ent::ParametrosDesenho::default_instance(), true  /*mundo*/);
 }
 
 void Modelos3d::DescarregaModelo3d(const std::string& id_interno) {
