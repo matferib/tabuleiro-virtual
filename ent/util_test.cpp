8f0154f51380e331de3b4ea03e524f878bd8eedd
Matheus Ribeiro <mfribeiro+tabuleirolinux@gmail.com>
2016-10-18 00:15:11 -0200

    teste de matriz

--- ent/util_test.cpp
+++ ent/util_test.cpp
@@ -19,6 +19,54 @@ TEST(TesteGeraDados, TesteGeraDados) {
   EXPECT_EQ(11, pv);
 }
 
+TEST(TesteMatriz, TesteMatriz) {
+  {
+    Vector3 v(1.0f, 0.0f, 0.0f); 
+    Vector3 vt(1.0f, 0.0f, 0.0f); 
+    Matrix4 m = MatrizRotacao(v);
+    EXPECT_EQ(v, m * vt);
+  }
+
+  {
+    Vector3 v(0.0f, 1.0f, 0.0f); 
+    Vector3 vt(1.0f, 0.0f, 0.0f); 
+    Matrix4 m = MatrizRotacao(v);
+    Vector3 vr = m * vt;
+    EXPECT_NEAR(v.x, vr.x, 0.001);
+    EXPECT_NEAR(v.y, vr.y, 0.001);
+    EXPECT_NEAR(v.z, vr.z, 0.001);
+  }
+
+  {
+    Vector3 v(0.0f, 0.0f, 1.0f); 
+    Vector3 vt(1.0f, 0.0f, 0.0f); 
+    Matrix4 m = MatrizRotacao(v);
+    Vector3 vr = m * vt;
+    EXPECT_NEAR(v.x, vr.x, 0.001);
+    EXPECT_NEAR(v.y, vr.y, 0.001);
+    EXPECT_NEAR(v.z, vr.z, 0.001);
+  }
+
+  {
+    Vector4 v1(4.0f, 1.0f, 5.0f, 1.0f); 
+    Vector4 v2(1.0f, 1.0f, 1.0f, 1.0f); 
+    Vector4 v3 = v1 - v2;  // da 3.0, 0.0, 4.0 (tamanho 5).
+    Vector4 vt(0.0f, 0.0f, 0.0f, 1.0f); 
+    Matrix4 m;
+    m.rotateY(90.0f);
+    m.rotateZ(180.0f);
+    m.translate(1.0f, 0.0f, 0.0f);
+    m.scale(5.0f, 0.0f, 0.0f);
+    m = MatrizRotacao(Vector3(v3.x, v3.y, v3.z)) * m;
+    m.translate(v2.x, v2.y, v2.z);
+    Vector4 vr = m * vt;
+    EXPECT_NEAR(v1.x, vr.x, 0.001);
+    EXPECT_NEAR(v1.y, vr.y, 0.001);
+    EXPECT_NEAR(v1.z, vr.z, 0.001);
+  }
+
+}
+
 }  // namespace ent.
 
 int main(int argc, char **argv) {
