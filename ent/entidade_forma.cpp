bbad055fd629f194226c2503b2b5058dafdbfeeb
Matheus Ribeiro <mfribeiro+planilha@gmail.com>
2016-10-18 23:38:05 -0200

    tirei stencil do desenho de oclusao

--- ent/entidade_forma.cpp
+++ ent/entidade_forma.cpp
@@ -307,7 +307,7 @@ void Entidade::DesenhaObjetoFormaProto(const EntidadeProto& proto,
       // Usar stencil nos dois casos (transparente ou solido) para que a cor do AjustaCor funcione.
       // caso contrario, ao atualizar a cor do desenho livre, o VBO tera que ser regerado.
       // Para picking, deve-se ignorar o stencil tb.
-      bool usar_stencil = !pd->desenha_mapa_sombras() && !pd->has_picking_x();
+      bool usar_stencil = !pd->desenha_mapa_sombras() && !pd->desenha_mapa_oclusao() && !pd->has_picking_x();
       if (usar_stencil) {
         LigaStencil();
       }
