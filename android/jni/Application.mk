76439bc84544149922967424803eddca80e0f1b7
Matheus Ribeiro <mfribeiro+tabuleirolinux@gmail.com>
2016-10-24 15:41:43 -0200

    clang

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -4,8 +4,8 @@ APP_CPPFLAGS := -std=c++11
 APP_ABI := armeabi-v7a
 APP_PLATFORM := android-17 # 4.4.2, 21 (5.0) da pau de rede.
 APP_STL := gnustl_shared
-NDK_TOOLCHAIN_VERSION := 4.9
-#NDK_TOOLCHAIN_VERSION := clang
+#NDK_TOOLCHAIN_VERSION := 4.9
+NDK_TOOLCHAIN_VERSION := clang
 ifeq ($(DEBUG),"1")
 APP_OPTIM := debug
 else
