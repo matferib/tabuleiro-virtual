3112143b8ab28c8d493f5313015587cfaaad467c
Matheus Windows Novo <mfribeiro@gmail.com>
2016-11-15 18:24:16 -0200

    forcando local de atributo por causa da amd

--- gltab/gl_comum.cpp
+++ gltab/gl_comum.cpp
@@ -7,10 +7,6 @@
 #include <vector>
 #include <map>
 
-#if USAR_GFLAGS
-#include <google/gflags.h>
-#endif
-
 #if __APPLE__
   #include "TargetConditionals.h"
   #if TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE
@@ -401,19 +397,29 @@ bool IniciaVariaveis(VarShader* shader) {
   }
 
   // Variaveis atributos.
-  for (const auto& d : std::vector<DadosVariavel> {
-          {"gltab_vertice", &shader->atr_gltab_vertice},
-          {"gltab_normal", &shader->atr_gltab_normal},
-          {"gltab_cor", &shader->atr_gltab_cor},
-          {"gltab_texel", &shader->atr_gltab_texel},
+  struct DadosAtributo {
+    DadosVariavel dv;
+    GLint indice;
+  };
+  for (const auto& d : std::vector<DadosAtributo> {
+          {{"gltab_vertice", &shader->atr_gltab_vertice}, 1},
+          {{"gltab_normal", &shader->atr_gltab_normal}, 2},
+          {{"gltab_cor", &shader->atr_gltab_cor}, 3},
+          {{"gltab_texel", &shader->atr_gltab_texel}, 4},
   }) {
-    *d.var = LeLocalAtributo(shader->programa, d.nome);
-    if (*d.var == -1) {
-      LOG(INFO) << "Shader nao possui atributo " << d.nome;
+    //*d.dv.var = LeLocalAtributo(shader->programa, d.dv.nome);
+    LocalAtributo(shader->programa, d.indice, d.dv.nome);
+    *d.dv.var = d.indice;
+    if (*d.dv.var == -1) {
+      LOG(INFO) << "Shader nao possui atributo " << d.dv.nome;
       continue;
     }
-    LOG(INFO) << "Atributo " << d.nome << " na posicao " << *d.var;
+    LOG(INFO) << "Atributo " << d.dv.nome << " na posicao " << *d.dv.var;
   }
+
+  // De novo.
+  LinkaPrograma(shader->programa);
+  V_ERRO_RET("linkando programa shader");
   return true;
 }
 
