ac238b4d6f1335acb1c369e3aab0e9cc72f562ee
Matheus Ribeiro <mfribeiro+tabuleirolinux@gmail.com>
2017-04-11 00:51:22 -0300

    oclusao shader preto branco

--- shaders/frag_preto_branco.c
+++ shaders/frag_preto_branco.c
@@ -1,4 +1,4 @@
-//#version 120
+#version ${VERSAO}
 
 #if defined(GL_ES)
 //precision highp float;
@@ -23,8 +23,29 @@ uniform highp vec4 gltab_nevoa_referencia;       // Ponto de referencia para com
 uniform lowp float gltab_textura;               // Textura ligada? 1.0 : 0.0
 uniform lowp sampler2D gltab_unidade_textura;   // handler da textura.
 
+uniform bool gltab_oclusao_ligada;          // true se oclusao estiver ligada.
+uniform highp float gltab_plano_distante_oclusao;  // distancia do plano de corte distante durante o mapeamento de oclusao.
+varying highp vec3 v_Pos_oclusao;  // Posicao do fragmento com relacao a primeira pesssoa.
+uniform highp samplerCube gltab_unidade_textura_oclusao;   // handler da textura do mapa da oclusao.
+
+lowp float Visivel(samplerCube sampler, vec3 pos) {
+  highp float bias = 0.5;
+  highp vec4 texprofcor = textureCube(sampler, pos, 0.0);
+  highp float mais_proximo = (texprofcor.r + (texprofcor.g / 256.0) + (texprofcor.b / 65536.0));
+  //gl_FragColor = vec4(mais_proximo, 0.0, 0.0, 1.0);
+  mais_proximo *= gltab_plano_distante_oclusao;
+  // Se mais_proximo menor que valor computado, retorna 0.
+  return step((length(pos) - bias), mais_proximo);
+}
+
 void main() {
   lowp vec4 cor_final = v_Color;
+  lowp vec4 cor_oclusao = vec4(1.0, 1.0, 1.0, 1.0);
+  if (gltab_oclusao_ligada) {
+    lowp float visivel = Visivel(gltab_unidade_textura_oclusao, v_Pos_oclusao);
+    cor_oclusao = vec4(visivel, visivel, visivel, 1.0);
+  }
+
   if (gltab_textura > 0.0) {
     cor_final *= texture2D(gltab_unidade_textura, v_Tex.st);
   }
@@ -37,5 +58,5 @@ void main() {
   if (peso_nevoa == 1.0) {
     discard;
   }
-  gl_FragColor = mix(cor_final, gltab_nevoa_cor, peso_nevoa);
+  gl_FragColor = mix(cor_final, gltab_nevoa_cor, peso_nevoa) * cor_oclusao;
 }
--- shaders/vert_preto_branco.c
+++ shaders/vert_preto_branco.c
@@ -1,4 +1,5 @@
-//# version 110
+#version ${VERSAO}
+
 // uniform: variaveis que nao variam durante primitiva.
 // attribute: variaveis que variam por vertice.
 
@@ -16,9 +17,11 @@
 varying lowp vec4 v_Color;
 varying lowp vec2 v_Tex;  // coordenada texel.
 varying highp vec4 v_Pos;  // posicao em coordenada de olho.
+varying highp vec3 v_Pos_oclusao;
 // Uniformes nao variam por vertice, vem de fora.
 uniform highp mat4 gltab_prm;    // projecao.
 uniform highp mat4 gltab_mvm;    // modelview.
+uniform highp mat4 gltab_mvm_oclusao;   // modelagem oclusao.
 uniform mediump vec4 gltab_dados_raster;    // p = tamanho ponto.
 // Atributos variam por vertice.
 attribute highp vec4 gltab_vertice;
@@ -31,4 +34,7 @@ void main() {
   v_Pos = gltab_mvm * gltab_vertice;
   gl_Position = gltab_prm * v_Pos;
   gl_PointSize = gltab_dados_raster.p;
+  // Oclusao.
+  highp vec4 pos_oclusao = gltab_mvm_oclusao * gltab_vertice;
+  v_Pos_oclusao = pos_oclusao.xyz / pos_oclusao.w;
 }
