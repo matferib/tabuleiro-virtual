03f51fb9e12258fc51ba866ebc47da462d157b9f
Matheus Ribeiro <mfribeiro+tabuleirolinux@gmail.com>
2018-04-07 17:11:40 -0300

    tirei x86 do qt do android pq nao consegui funcionar

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,11 +1,16 @@
 # Baseado na documentacao em: file:///opt/android-ndk-r9d/docs/APPLICATION-MK.html.
 #APP_MODULES := tabuleiro protobuf-prebuilt boost-prebuilt
 APP_CPPFLAGS := -std=c++11
-APP_ABI := x86 armeabi-v7a
+
+ifneq ($(USAR_QT),)
+	APP_ABI := armeabi-v7a
+else
+	APP_ABI := x86 armeabi-v7a
+endif
 APP_PLATFORM := android-17 # 4.4.2, 21 (5.0) da pau de rede.
 APP_STL := gnustl_shared
-#NDK_TOOLCHAIN_VERSION := 4.9
 NDK_TOOLCHAIN_VERSION := clang
+
 ifeq ($(DEBUG),"1")
 APP_OPTIM := debug
 else
