6e4e58746671624da1752fcb28eb3b39b83ec056
Matheus Ribeiro <mfribeiro+tabuleirolinux@gmail.com>
2018-04-07 17:09:48 -0300

    condicoes no Android.mk para qt

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -1,5 +1,25 @@
 LOCAL_PATH := $(call my-dir)
 
+# Qt libs.
+ifneq ($(USAR_QT),)
+	include $(CLEAR_VARS)
+	LOCAL_MODULE := qt5core-prebuilt
+	LOCAL_SRC_FILES := ../../../../Qt5/5.10.1/android_$(TARGET_ARCH_ABI)/lib/libQt5Core.so
+	include $(PREBUILT_SHARED_LIBRARY)
+	include $(CLEAR_VARS)
+	LOCAL_MODULE := qt5gui-prebuilt
+	LOCAL_SRC_FILES := ../../../../Qt5/5.10.1/android_$(TARGET_ARCH_ABI)/lib/libQt5Gui.so
+	include $(PREBUILT_SHARED_LIBRARY)
+	include $(CLEAR_VARS)
+	LOCAL_MODULE := qt5widgets-prebuilt
+	LOCAL_SRC_FILES := ../../../../Qt5/5.10.1/android_$(TARGET_ARCH_ABI)/lib/libQt5Widgets.so
+	include $(PREBUILT_SHARED_LIBRARY)
+	include $(CLEAR_VARS)
+	LOCAL_MODULE := qt5opengl-prebuilt
+	LOCAL_SRC_FILES := ../../../../Qt5/5.10.1/android_$(TARGET_ARCH_ABI)/lib/libQt5OpenGL.so
+	include $(PREBUILT_SHARED_LIBRARY)
+endif
+
 # Proto: precisa gerar o config.
 include $(CLEAR_VARS)
 LOCAL_MODULE := protobuf
@@ -29,16 +49,31 @@ include $(BUILD_STATIC_LIBRARY)
 # Baseado em file:///opt/android-ndk-r9d/docs/ANDROID-MK.html.
 include $(CLEAR_VARS)         # Limpa todas variaveis LOCAL_* exceto LOCAL_PATH.
 LOCAL_MODULE := tabuleiro     # Modulo definido por este Android.mk.
+
+ifneq ($(USAR_QT),)
+	LOCAL_C_INCLUDES := /home/matheus/Qt5/5.10.1/android_armv7/include/ \
+                      /home/matheus/Qt5/5.10.1/android_armv7/include/QtWidgets \
+                      /home/matheus/Qt5/5.10.1/android_armv7/include/QtGui \
+                      /home/matheus/Qt5/5.10.1/android_armv7/include/QtOpenGL \
+                      /home/matheus/Qt5/5.10.1/android_armv7/include/QtCore
+endif
+
 LOCAL_SRC_FILES := jni-impl.cpp \
 	                 gltab/gl_es.cpp gltab/gl_comum.cpp gltab/gl_char.cpp gltab/gl_vbo.cpp gltab/glues.cpp \
 									 matrix/matrices.cpp \
 									 net/util.cpp net/cliente.cpp net/servidor.cpp net/socket.cpp \
 									 ntf/notificacao.cpp ntf/notificacao.pb.cpp \
-									 ent/constantes.cpp ent/entidade.pb.cpp ent/tabuleiro.pb.cpp ent/acoes.pb.cpp ent/controle_virtual.pb.cpp ent/entidade.cpp ent/entidade_desenho.cpp ent/entidade_composta.cpp ent/entidade_forma.cpp ent/tabuleiro.cpp ent/tabuleiro_controle_virtual.cpp ent/tabuleiro_picking.cpp ent/acoes.cpp ent/util.cpp ent/tabuleiro_interface.cpp ent/tabuleiro_tratador.cpp ent/comum.pb.cpp ent/tabelas.cpp ent/tabelas.pb.cpp \
+									 ent/constantes.cpp ent/entidade.pb.cpp ent/tabuleiro.pb.cpp ent/acoes.pb.cpp ent/controle_virtual.pb.cpp \
+									 ent/entidade.cpp ent/entidade_desenho.cpp ent/entidade_composta.cpp ent/entidade_forma.cpp ent/tabuleiro.cpp \
+									 ent/tabuleiro_controle_virtual.cpp ent/tabuleiro_picking.cpp ent/acoes.cpp ent/util.cpp \
+									 ent/tabuleiro_interface.cpp ent/tabuleiro_tratador.cpp ent/comum.pb.cpp ent/tabelas.cpp ent/tabelas.pb.cpp \
                    ifg/tecladomouse.cpp ifg/interface.cpp ifg/interface_android.cpp ifg/modelos.pb.cc \
                    tex/texturas.cpp tex/lodepng.cpp \
 									 m3d/m3d.cpp \
 									 arq/arquivo.cpp arq/arquivo_android.cpp
+ifneq ($(USAR_QT),)
+	LOCAL_SRC_FILES += ifg/qt/qt_interface.cpp ifg/qt/util.cpp ifg/qt/moc_util.cpp
+endif
 
 # Flags de performance.
 LOCAL_ARM_MODE := arm
@@ -48,6 +83,9 @@ LOCAL_ARM_MODE := arm
 #	LOCAL_ARM_NEON := true
 #endif
 
+ifneq ($(USAR_QT),)
+	LOCAL_CPPFLAGS += -DUSAR_OPENGL_ES -DUSAR_QT -DUSAR_QT5
+endif
 LOCAL_CPPFLAGS += -DUSAR_OPENGL_ES -DDEBUG=$(DEBUG)
 ifneq ($(NEXUS7),)
 	LOCAL_CPPFLAGS += -DZBUFFER_16_BITS
@@ -56,6 +94,10 @@ ifneq ($(XOOM),)
 	LOCAL_CPPFLAGS += -DZBUFFER_16_BITS
 endif
 LOCAL_STATIC_LIBRARIES := protobuf boost
+
+ifneq ($(USAR_QT),)
+	LOCAL_SHARED_LIBRARIES := qt5core-prebuilt qt5gui-prebuilt qt5widgets-prebuilt qt5opengl-prebuilt
+endif
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog -landroid
 LOCAL_CPP_FEATURES := rtti exceptions
 
