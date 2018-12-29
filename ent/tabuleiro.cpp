93f7b49a4dbb2e4ce7436175e832a97d671a71e1
Matheus Ribeiro <mfribeiro@gmail.com>
2018-12-29 00:33:54 -0200

    cura acelerada

--- ent/tabuleiro.cpp
+++ ent/tabuleiro.cpp
@@ -7027,6 +7027,7 @@ void Tabuleiro::AtualizaMovimentoAoPassarRodada(const Entidade& entidade, ntf::N
 void Tabuleiro::AtualizaCuraAceleradaAoPassarRodada(const Entidade& entidade, ntf::Notificacao* grupo) {
   if (entidade.PontosVida() >= entidade.MaximoPontosVida() && entidade.DanoNaoLetal() == 0) return;
   PreencheNotificacaoCuraAcelerada(entidade, grupo->add_notificacao());
+  AdicionaAcaoDeltaPontosVidaSemAfetar(entidade.Id(), CuraAcelerada(entidade.Proto()));
 }
 
 void Tabuleiro::ReiniciaAtaqueAoPassarRodada(const Entidade& entidade, ntf::Notificacao* grupo) {
