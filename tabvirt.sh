644a0ae82315970b2210a4d4174ec6cc736d46b9
Matheus Ribeiro <mfribeiro@gmail.com>
2019-01-05 15:03:10 -0200

    script com debug

--- tabvirt.sh
+++ tabvirt.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
 export LD_LIBRARY_PATH=/home/matheus/Projetos/libs/Qt/5.11.1/gcc_64/lib
-#gdb -tui --args ./tabvirt $@
-#lldb ./tabvirt $@
+
+if [ "$1" == "debug" ]; then
+gdb -tui ./tabvirt
+#lldb ./tabvirt
+else
 ./tabvirt $@
+fi
