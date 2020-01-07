d9cf570ad76c6211b57be90398f50a139242da11
Matheus Ribeiro <mfribeiro@gmail.com>
2020-01-07 01:15:55 -0300

    especularidade funcionando

--- shaders/frag_luz_espec.c
+++ shaders/frag_luz_espec.c
@@ -72,16 +72,23 @@ uniform highp vec4 gltab_nevoa_referencia;      // Ponto de referencia para comp
 //uniform bool gltab_stencil;              // Stencil ligado?
 uniform highp mat3 gltab_nm;     // normal matrix
 
-lowp vec4 CorLuzDirecional(in lowp vec3 vetor_referencia, in InfoLuzDirecional luz_direcional) {
+lowp vec4 CorLuzDirecionalEspecular(in lowp vec3 vetor_referencia, in InfoLuzDirecional luz_direcional) {
   highp vec3 direcao_luz = vec3(luz_direcional.pos * (1 / luz_direcional.pos.w));
   // dot(v1 v2) = cos(angulo) * |v1| * |v2|.
-  lowp float cos_ref = dot(vetor_referencia, direcao_luz);
-  if (gltab_especularidade_ligada) {
-    cos_ref = pow(cos_ref, 3);
-  }
+  lowp float cos_ref = max(0.0, dot(vetor_referencia, direcao_luz));
+  cos_ref = pow(cos_ref, 8);
   lowp vec4 cor_final = clamp(luz_direcional.cor * cos_ref, 0.0, 1.0);
   return cor_final * step(0.1, luz_direcional.cor.a);
 }
+
+lowp vec4 CorLuzDirecionalDifusa(in lowp vec3 vetor_referencia, in InfoLuzDirecional luz_direcional) {
+  highp vec3 direcao_luz = vec3(luz_direcional.pos * (1 / luz_direcional.pos.w));
+  // dot(v1 v2) = cos(angulo) * |v1| * |v2|.
+  lowp float cos_ref = max(0.0, dot(vetor_referencia, direcao_luz));
+  lowp vec4 cor_final = clamp(luz_direcional.cor * cos_ref, 0.0, 1.0);
+  return cor_final * step(0.1, luz_direcional.cor.a);
+}
+
 
 lowp float Visivel(samplerCube sampler, highp vec3 pos) {
   highp float bias = 0.5;
@@ -93,20 +100,34 @@ lowp float Visivel(samplerCube sampler, highp vec3 pos) {
   return step((length(pos) - bias), mais_proximo);
 }
 
-lowp vec4 CorLuzPontual(in bool testa_visibilidade, in lowp vec3 vetor_referencia, in InfoLuzPontual luz) {
+lowp vec4 CorLuzPontualEspecular(in bool testa_visibilidade, in lowp vec3 vetor_referencia, in InfoLuzPontual luz) {
   if (luz.cor.a == 0.0) return vec4(0.0);
   // Vetor objeto luz.
   highp vec4 objeto_luz_homogeneo = (luz.pos / luz.pos.w) - v_Pos;
   highp vec3 objeto_luz = objeto_luz_homogeneo.xyz;
-  highp float tam = length(objeto_luz);
-  lowp float atenuacao = gltab_especularidade_ligada
-    ? step(tam, luz.atributos.r) * (tam / luz.atributos.r)  // atenuacao linear no raio.
-    : (0.5 * step(tam, luz.atributos.r) + 0.5 * step(tam, luz.atributos.r * 2.0));  // sem atenuacao por distancia ate raio, depois metade da atenuacao.
+  highp float distancia_objeto_luz = length(objeto_luz);
+  lowp float atenuacao =
+    // Atenuacao zero ate raio, depois acaba quase repentinamente.
+    (1.0 - smoothstep(luz.atributos.r * 4, luz.atributos.r * 4 + 0.1, distancia_objeto_luz));
   // dot(v1 v2) = cos(angulo) * |v1| * |v2|.
-  lowp float cos_ref = dot(vetor_referencia, normalize(objeto_luz));
-  if (gltab_especularidade_ligada) {
-    cos_ref = pow(cos_ref, 3);
-  }
+  lowp float cos_ref = max(0.0, dot(vetor_referencia, normalize(objeto_luz)));
+  //cos_ref = smoothstep(0.95, 0.98, cos_ref);
+  cos_ref = pow(cos_ref, 8);
+  lowp float visibilidade = testa_visibilidade ? Visivel(gltab_unidade_textura_luz, v_Pos_luz) : 1.0;
+  return visibilidade * clamp(luz.cor * cos_ref, 0.0, 1.0) * atenuacao;
+}
+
+lowp vec4 CorLuzPontualDifusa(in bool testa_visibilidade, in lowp vec3 vetor_referencia, in InfoLuzPontual luz) {
+  if (luz.cor.a == 0.0) return vec4(0.0);
+  // Vetor objeto luz.
+  highp vec4 objeto_luz_homogeneo = (luz.pos / luz.pos.w) - v_Pos;
+  highp vec3 objeto_luz = objeto_luz_homogeneo.xyz;
+  highp float distancia_objeto_luz = length(objeto_luz);
+  lowp float atenuacao =
+    // Sem atenuacao ate raio, depois meia luz ate raio final.
+    (0.5 * step(distancia_objeto_luz, luz.atributos.r) + 0.5 * step(distancia_objeto_luz, luz.atributos.r * 2.0));
+  // dot(v1 v2) = cos(angulo) * |v1| * |v2|.
+  lowp float cos_ref = max(0.0, dot(vetor_referencia, normalize(objeto_luz)));
   lowp float visibilidade = testa_visibilidade ? Visivel(gltab_unidade_textura_luz, v_Pos_luz) : 1.0;
   return visibilidade * clamp(luz.cor * cos_ref, 0.0, 1.0) * atenuacao;
 }
@@ -117,6 +138,10 @@ void main() {
   if (gltab_oclusao_ligada) {
     lowp float visivel = Visivel(gltab_unidade_textura_oclusao, v_Pos_oclusao);
     cor_oclusao = vec4(visivel, visivel, visivel, 1.0);
+  }
+
+  if (gltab_textura > 0.0) {
+    cor_final *= texture2D(gltab_unidade_textura, v_Tex.st);
   }
 
   // luz ambiente.
@@ -148,20 +173,31 @@ void main() {
     } else {
       vetor_referencia = v_Normal;
     }
-    lowp mat4 cor_luz = mat4(aplicar_luz_direcional * CorLuzDirecional(vetor_referencia, gltab_luz_direcional),
-                             CorLuzPontual(true, vetor_referencia, gltab_luzes[0]),
-                             CorLuzPontual(false, vetor_referencia, gltab_luzes[1]),
-                             CorLuzPontual(false, vetor_referencia, gltab_luzes[2]));
-    lowp mat4 cor_luz_2 = mat4(CorLuzPontual(false, vetor_referencia, gltab_luzes[3]),
-                               CorLuzPontual(false, vetor_referencia, gltab_luzes[4]),
-                               CorLuzPontual(false, vetor_referencia, gltab_luzes[5]),
-                               CorLuzPontual(false, vetor_referencia, gltab_luzes[6]));
-    cor_final *= clamp(gltab_luz_ambiente + cor_luz * uns + cor_luz_2 * uns, 0.0, 1.0);
-  }
+    lowp mat4 cor_luzes_difusas_1 = mat4(aplicar_luz_direcional * CorLuzDirecionalDifusa(vetor_referencia, gltab_luz_direcional),
+                                         CorLuzPontualDifusa(true, vetor_referencia, gltab_luzes[0]),
+                                         CorLuzPontualDifusa(false, vetor_referencia, gltab_luzes[1]),
+                                         CorLuzPontualDifusa(false, vetor_referencia, gltab_luzes[2]));
+    lowp mat4 cor_luzes_difusas_2 = mat4(CorLuzPontualDifusa(false, vetor_referencia, gltab_luzes[3]),
+                                         CorLuzPontualDifusa(false, vetor_referencia, gltab_luzes[4]),
+                                         CorLuzPontualDifusa(false, vetor_referencia, gltab_luzes[5]),
+                                         CorLuzPontualDifusa(false, vetor_referencia, gltab_luzes[6]));
+    cor_final *= clamp(gltab_luz_ambiente + cor_luzes_difusas_1 * uns + cor_luzes_difusas_2 * uns, 0.0, 1.0);
+    if (gltab_especularidade_ligada) {
+      lowp mat4 cor_luzes_especulares_1 = mat4(aplicar_luz_direcional * CorLuzDirecionalEspecular(vetor_referencia, gltab_luz_direcional),
+                                               CorLuzPontualEspecular(true, vetor_referencia, gltab_luzes[0]),
+                                               CorLuzPontualEspecular(false, vetor_referencia, gltab_luzes[1]),
+                                               CorLuzPontualEspecular(false, vetor_referencia, gltab_luzes[2]));
+      lowp mat4 cor_luzes_especulares_2 = mat4(CorLuzPontualEspecular(false, vetor_referencia, gltab_luzes[3]),
+                                               CorLuzPontualEspecular(false, vetor_referencia, gltab_luzes[4]),
+                                               CorLuzPontualEspecular(false, vetor_referencia, gltab_luzes[5]),
+                                               CorLuzPontualEspecular(false, vetor_referencia, gltab_luzes[6]));
 
-  if (gltab_textura > 0.0) {
-    cor_final *= texture2D(gltab_unidade_textura, v_Tex.st);
+      cor_final = clamp(cor_final + cor_luzes_especulares_1 * uns + cor_luzes_especulares_2 * uns, 0.0, 1.0);
+    }
   }
+  //mediump float gamma_correction = 1.0 / 2.2;
+  //mediump vec4 gamma = vec4(gamma_correction, gamma_correction, gamma_correction, 1.0);
+  //cor_final = pow(cor_final, gamma);
 
   // Nevoa.
   cor_final *= gltab_cor_mistura_pre_nevoa * cor_oclusao;
