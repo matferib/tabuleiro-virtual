83addc2c31feb66839dcc1a3855d0f4cd2ad19d9
Matheus Ribeiro <mfribeiro@gmail.com>
2020-01-07 23:38:58 -0300

    correcoes no shader especular

--- shaders/frag_luz_espec.c
+++ shaders/frag_luz_espec.c
@@ -72,10 +72,10 @@ uniform highp vec4 gltab_nevoa_referencia;      // Ponto de referencia para comp
 //uniform bool gltab_stencil;              // Stencil ligado?
 uniform highp mat3 gltab_nm;     // normal matrix
 
-lowp vec4 CorLuzDirecionalEspecular(in lowp vec3 vetor_referencia, in InfoLuzDirecional luz_direcional) {
+lowp vec4 CorLuzDirecionalEspecular(in lowp vec3 vetor_olho_objeto_refletido, in InfoLuzDirecional luz_direcional) {
   highp vec3 direcao_luz = vec3(luz_direcional.pos * (1 / luz_direcional.pos.w));
   // dot(v1 v2) = cos(angulo) * |v1| * |v2|.
-  lowp float cos_ref = max(0.0, dot(vetor_referencia, direcao_luz));
+  lowp float cos_ref = max(0.0, dot(vetor_olho_objeto_refletido, direcao_luz));
   cos_ref = pow(cos_ref, 8);
   lowp vec4 cor_final = clamp(luz_direcional.cor * cos_ref, 0.0, 1.0);
   return cor_final * step(0.1, luz_direcional.cor.a);
@@ -89,7 +89,72 @@ lowp vec4 CorLuzDirecionalDifusa(in lowp vec3 vetor_referencia, in InfoLuzDireci
   return cor_final * step(0.1, luz_direcional.cor.a);
 }
 
+lowp float AtenuacaoLuz(in mediump float distancia_objeto_luz, in InfoLuzPontual luz) {
+  if (luz.cor.a == 0.0) return 0.0;
+  lowp float distancia_pos_raio = max(0.0, distancia_objeto_luz - luz.atributos.r);
+  return 1.0 - smoothstep(0, luz.atributos.r, distancia_pos_raio);
+  // Algoritmo real: atenuacao quadratuca apos raio.
+    //1.0 / (1.0 + pow(distancia_pos_raio, 2));
+  // Melhor para D&D.
+  // Sem atenuacao ate raio, depois meia luz ate raio final.
+    //1.0 / (1 + pow(clamp(distancia_objeto_luz - luz.atributos.r, 0, distancia_objeto_luz), 2));
+    //(0.5 * step(distancia_objeto_luz, luz.atributos.r) + 0.5 * step(distancia_objeto_luz, luz.atributos.r * 2.0));
+}
 
+mediump vec4 VetorLuzObjeto(in InfoLuzPontual luz) {
+  if (luz.cor.a == 0.0) return vec4(0.0);
+  // Vetor objeto luz.
+  return (luz.pos / luz.pos.w) - v_Pos;
+}
+
+lowp vec4 CorLuzPontualEspecular(in lowp float atenuacao, in lowp vec3 vetor_olho_objeto_refletido, in InfoLuzPontual luz) {
+  if (luz.cor.a == 0.0) return vec4(0.0);
+  // Vetor objeto luz.
+  highp vec4 objeto_luz_homogeneo = (luz.pos / luz.pos.w) - v_Pos;
+  highp vec3 objeto_luz = objeto_luz_homogeneo.xyz;
+  highp float distancia_objeto_luz = length(objeto_luz);
+  // dot(v1 v2) = cos(angulo) * |v1| * |v2|.
+  lowp float cos_ref = max(0.0, dot(vetor_olho_objeto_refletido, normalize(objeto_luz)));
+  //cos_ref = smoothstep(0.95, 0.98, cos_ref);
+  cos_ref = pow(cos_ref, 8);
+  return atenuacao * cos_ref * luz.cor * cos_ref;
+}
+
+lowp vec4 CorLuzPontualDifusa(
+    in lowp float atenuacao, in lowp vec3 vetor_referencia, in InfoLuzPontual luz) {
+  if (luz.cor.a == 0.0) return vec4(0.0);
+  // Vetor objeto luz.
+  highp vec4 objeto_luz_homogeneo = (luz.pos / luz.pos.w) - v_Pos;
+  highp vec3 objeto_luz = objeto_luz_homogeneo.xyz;
+  highp float distancia_objeto_luz = length(objeto_luz);
+  // dot(v1 v2) = cos(angulo) * |v1| * |v2|.
+  lowp float cos_ref = max(0.0, dot(vetor_referencia, normalize(objeto_luz)));
+  return luz.cor * cos_ref * atenuacao;
+}
+
+lowp float VisibilidadeLuzDirecional() {
+  //lowp vec4 cor_luz = gltab_luz_ambiente;
+  highp float cos_theta = clamp(dot(v_Normal, gltab_luz_direcional.pos.xyz), 0.0, 1.0);
+  highp float bias = 0.002 * tan(acos(cos_theta));
+  bias = clamp(bias, 0.00, 0.0035);
+#if __VERSION__ == 130
+  lowp float aplicar_luz_direcional = texture(gltab_unidade_textura_sombra, vec3(v_Pos_sombra.xy, v_Pos_sombra.z - bias));
+#elif __VERSION__ == 120
+  lowp float aplicar_luz_direcional = shadow2D(gltab_unidade_textura_sombra, vec3(v_Pos_sombra.xy, v_Pos_sombra.z - bias)).r;
+#elif defined(GL_EXT_shadow_samplers)
+  lowp float aplicar_luz_direcional = shadow2DEXT(
+      gltab_unidade_textura_sombra, vec3(v_Pos_sombra.xy, v_Pos_sombra.z - bias));
+#else
+  // OpenGL ES 2.0.
+  lowp vec4 texprofcor = texture2D(gltab_unidade_textura_sombra, v_Pos_sombra.xy);
+  lowp float texz = texprofcor.r + (texprofcor.g / 256.0) + (texprofcor.b / 65536.0);
+  // Se texz menor que valor computado, retorna 0.
+  lowp float aplicar_luz_direcional = step((v_Pos_sombra.z - bias), texz);
+#endif
+  return aplicar_luz_direcional;
+}
+
+// Retorna quao visivel o ponto eh para a luz (mapeamento de sombras).
 lowp float Visivel(samplerCube sampler, highp vec3 pos) {
   highp float bias = 0.5;
   highp vec4 texprofcor = textureCube(sampler, pos, 0.0);
@@ -100,98 +165,105 @@ lowp float Visivel(samplerCube sampler, highp vec3 pos) {
   return step((length(pos) - bias), mais_proximo);
 }
 
-lowp vec4 CorLuzPontualEspecular(in bool testa_visibilidade, in lowp vec3 vetor_referencia, in InfoLuzPontual luz) {
-  if (luz.cor.a == 0.0) return vec4(0.0);
-  // Vetor objeto luz.
-  highp vec4 objeto_luz_homogeneo = (luz.pos / luz.pos.w) - v_Pos;
-  highp vec3 objeto_luz = objeto_luz_homogeneo.xyz;
-  highp float distancia_objeto_luz = length(objeto_luz);
-  lowp float atenuacao =
-    // Atenuacao zero ate raio, depois acaba quase repentinamente.
-    (1.0 - smoothstep(luz.atributos.r * 4, luz.atributos.r * 4 + 0.1, distancia_objeto_luz));
-  // dot(v1 v2) = cos(angulo) * |v1| * |v2|.
-  lowp float cos_ref = max(0.0, dot(vetor_referencia, normalize(objeto_luz)));
-  //cos_ref = smoothstep(0.95, 0.98, cos_ref);
-  cos_ref = pow(cos_ref, 8);
-  lowp float visibilidade = testa_visibilidade ? Visivel(gltab_unidade_textura_luz, v_Pos_luz) : 1.0;
-  return visibilidade * clamp(luz.cor * cos_ref, 0.0, 1.0) * atenuacao;
-}
-
-lowp vec4 CorLuzPontualDifusa(in bool testa_visibilidade, in lowp vec3 vetor_referencia, in InfoLuzPontual luz) {
-  if (luz.cor.a == 0.0) return vec4(0.0);
-  // Vetor objeto luz.
-  highp vec4 objeto_luz_homogeneo = (luz.pos / luz.pos.w) - v_Pos;
-  highp vec3 objeto_luz = objeto_luz_homogeneo.xyz;
-  highp float distancia_objeto_luz = length(objeto_luz);
-  lowp float atenuacao =
-    // Sem atenuacao ate raio, depois meia luz ate raio final.
-    (0.5 * step(distancia_objeto_luz, luz.atributos.r) + 0.5 * step(distancia_objeto_luz, luz.atributos.r * 2.0));
-  // dot(v1 v2) = cos(angulo) * |v1| * |v2|.
-  lowp float cos_ref = max(0.0, dot(vetor_referencia, normalize(objeto_luz)));
-  lowp float visibilidade = testa_visibilidade ? Visivel(gltab_unidade_textura_luz, v_Pos_luz) : 1.0;
-  return visibilidade * clamp(luz.cor * cos_ref, 0.0, 1.0) * atenuacao;
-}
-
 void main() {
   lowp vec4 cor_final = v_Color;
+
+  // Se houver oclusao, verifica se o ponto é visivel. Se nao for, corrige a oclusao.
   lowp vec4 cor_oclusao = vec4(1.0, 1.0, 1.0, 1.0);
   if (gltab_oclusao_ligada) {
     lowp float visivel = Visivel(gltab_unidade_textura_oclusao, v_Pos_oclusao);
     cor_oclusao = vec4(visivel, visivel, visivel, 1.0);
   }
 
+  // Aplica textura.
   if (gltab_textura > 0.0) {
     cor_final *= texture2D(gltab_unidade_textura, v_Tex.st);
   }
 
-  // luz ambiente.
+  // luzes.
   if (gltab_luz_ambiente.a > 0.0) {
-    //lowp vec4 cor_luz = gltab_luz_ambiente;
-    highp float cos_theta = clamp(dot(v_Normal, gltab_luz_direcional.pos.xyz), 0.0, 1.0);
-    highp float bias = 0.002 * tan(acos(cos_theta));
-    bias = clamp(bias, 0.00, 0.0035);
-#if __VERSION__ == 130
-    lowp float aplicar_luz_direcional = texture(gltab_unidade_textura_sombra, vec3(v_Pos_sombra.xy, v_Pos_sombra.z - bias));
-#elif __VERSION__ == 120
-    lowp float aplicar_luz_direcional = shadow2D(gltab_unidade_textura_sombra, vec3(v_Pos_sombra.xy, v_Pos_sombra.z - bias)).r;
-#elif defined(GL_EXT_shadow_samplers)
-    lowp float aplicar_luz_direcional = shadow2DEXT(
-        gltab_unidade_textura_sombra, vec3(v_Pos_sombra.xy, v_Pos_sombra.z - bias));
-#else
-    // OpenGL ES 2.0.
-    lowp vec4 texprofcor = texture2D(gltab_unidade_textura_sombra, v_Pos_sombra.xy);
-    lowp float texz = texprofcor.r + (texprofcor.g / 256.0) + (texprofcor.b / 65536.0);
-    // Se texz menor que valor computado, retorna 0.
-    lowp float aplicar_luz_direcional = step((v_Pos_sombra.z - bias), texz);
-#endif
-    // Outras luzes.
+    // Auxilia algumas contas.
     lowp vec4 uns = vec4(1.0, 1.0, 1.0, 1.0);
-    lowp vec3 vetor_referencia;
-    if (gltab_especularidade_ligada) {
-      // A camera esta em 0, entao o vetor olho objeto e simplesmente o objeto.
-      vetor_referencia = normalize(reflect(vec3(v_Pos), v_Normal));
-    } else {
-      vetor_referencia = v_Normal;
-    }
-    lowp mat4 cor_luzes_difusas_1 = mat4(aplicar_luz_direcional * CorLuzDirecionalDifusa(vetor_referencia, gltab_luz_direcional),
-                                         CorLuzPontualDifusa(true, vetor_referencia, gltab_luzes[0]),
-                                         CorLuzPontualDifusa(false, vetor_referencia, gltab_luzes[1]),
-                                         CorLuzPontualDifusa(false, vetor_referencia, gltab_luzes[2]));
-    lowp mat4 cor_luzes_difusas_2 = mat4(CorLuzPontualDifusa(false, vetor_referencia, gltab_luzes[3]),
-                                         CorLuzPontualDifusa(false, vetor_referencia, gltab_luzes[4]),
-                                         CorLuzPontualDifusa(false, vetor_referencia, gltab_luzes[5]),
-                                         CorLuzPontualDifusa(false, vetor_referencia, gltab_luzes[6]));
-    cor_final *= clamp(gltab_luz_ambiente + cor_luzes_difusas_1 * uns + cor_luzes_difusas_2 * uns, 0.0, 1.0);
-    if (gltab_especularidade_ligada) {
-      lowp mat4 cor_luzes_especulares_1 = mat4(aplicar_luz_direcional * CorLuzDirecionalEspecular(vetor_referencia, gltab_luz_direcional),
-                                               CorLuzPontualEspecular(true, vetor_referencia, gltab_luzes[0]),
-                                               CorLuzPontualEspecular(false, vetor_referencia, gltab_luzes[1]),
-                                               CorLuzPontualEspecular(false, vetor_referencia, gltab_luzes[2]));
-      lowp mat4 cor_luzes_especulares_2 = mat4(CorLuzPontualEspecular(false, vetor_referencia, gltab_luzes[3]),
-                                               CorLuzPontualEspecular(false, vetor_referencia, gltab_luzes[4]),
-                                               CorLuzPontualEspecular(false, vetor_referencia, gltab_luzes[5]),
-                                               CorLuzPontualEspecular(false, vetor_referencia, gltab_luzes[6]));
 
+    // Vetores de objeto-luz para cada luz.
+    mediump mat4 vetores_luz_objeto_1 = mat4(
+        vec4(0.0),  // nao usado.
+        VetorLuzObjeto(gltab_luzes[0]),
+        VetorLuzObjeto(gltab_luzes[1]),
+        VetorLuzObjeto(gltab_luzes[2]));
+    mediump mat4 vetores_luz_objeto_2 = mat4(
+        VetorLuzObjeto(gltab_luzes[3]),
+        VetorLuzObjeto(gltab_luzes[4]),
+        VetorLuzObjeto(gltab_luzes[5]),
+        VetorLuzObjeto(gltab_luzes[6]));
+
+    // Modulos dos vetores usado para atenuacao.
+    mediump vec4 modulos_1 = vec4(
+        0.0  /*nao usado*/,
+        length(vetores_luz_objeto_1[1]),
+        length(vetores_luz_objeto_1[2]),
+        length(vetores_luz_objeto_1[3]));
+    mediump vec4 modulos_2 = vec4(
+        length(vetores_luz_objeto_2[0]),
+        length(vetores_luz_objeto_2[1]),
+        length(vetores_luz_objeto_2[2]),
+        length(vetores_luz_objeto_2[3]));
+
+    // Atenuacoes.
+    lowp vec4 atenuacoes_1 = vec4(
+        0.0,  // nao usado.
+        AtenuacaoLuz(modulos_1[1], gltab_luzes[0]),
+        AtenuacaoLuz(modulos_1[2], gltab_luzes[1]),
+        AtenuacaoLuz(modulos_1[3], gltab_luzes[2]));
+    lowp vec4 atenuacoes_2 = vec4(
+        AtenuacaoLuz(modulos_2[0], gltab_luzes[3]),
+        AtenuacaoLuz(modulos_2[1], gltab_luzes[4]),
+        AtenuacaoLuz(modulos_2[2], gltab_luzes[5]),
+        AtenuacaoLuz(modulos_2[3], gltab_luzes[6]));
+
+    // Visibilidade do ponto (mapeamento de sombras).
+    // Obs: deixando pronto para quando tiver mais.
+    lowp vec4 visibilidades_1 = vec4(
+        VisibilidadeLuzDirecional(),
+        Visivel(gltab_unidade_textura_luz, v_Pos_luz),
+        1.0,
+        1.0);
+    lowp vec4 visibilidades_2 = vec4(
+        1.0,
+        1.0,
+        1.0,
+        1.0);
+
+    // As luzes.
+    lowp mat4 cor_luzes_difusas_1 = mat4(
+        visibilidades_1[0] * CorLuzDirecionalDifusa(v_Normal, gltab_luz_direcional),
+        visibilidades_1[1] * CorLuzPontualDifusa(atenuacoes_1[1], v_Normal, gltab_luzes[0]),
+        visibilidades_1[2] * CorLuzPontualDifusa(atenuacoes_1[2], v_Normal, gltab_luzes[1]),
+        visibilidades_1[3] * CorLuzPontualDifusa(atenuacoes_1[3], v_Normal, gltab_luzes[2]));
+    lowp mat4 cor_luzes_difusas_2 = mat4(
+        visibilidades_2[0] * CorLuzPontualDifusa(atenuacoes_2[0], v_Normal, gltab_luzes[3]),
+        visibilidades_2[1] * CorLuzPontualDifusa(atenuacoes_2[1], v_Normal, gltab_luzes[4]),
+        visibilidades_2[2] * CorLuzPontualDifusa(atenuacoes_2[2], v_Normal, gltab_luzes[5]),
+        visibilidades_2[3] * CorLuzPontualDifusa(atenuacoes_2[3], v_Normal, gltab_luzes[6]));
+
+    // Cor difusa e ambiente aplicada.
+    cor_final *= gltab_luz_ambiente + cor_luzes_difusas_1 * uns + cor_luzes_difusas_2 * uns;
+
+    // Aplica especularidade.
+    if (gltab_especularidade_ligada) {
+      lowp vec3 vetor_olho_objeto_refletido = normalize(reflect(vec3(v_Pos), v_Normal));
+      lowp mat4 cor_luzes_especulares_1 = mat4(
+          visibilidades_1[0] * CorLuzDirecionalEspecular(vetor_olho_objeto_refletido, gltab_luz_direcional),
+          visibilidades_1[1] * CorLuzPontualEspecular(atenuacoes_1[1], vetor_olho_objeto_refletido, gltab_luzes[0]),
+          visibilidades_1[2] * CorLuzPontualEspecular(atenuacoes_1[2], vetor_olho_objeto_refletido, gltab_luzes[1]),
+          visibilidades_1[3] * CorLuzPontualEspecular(atenuacoes_1[3], vetor_olho_objeto_refletido, gltab_luzes[2]));
+      lowp mat4 cor_luzes_especulares_2 = mat4(
+          CorLuzPontualEspecular(atenuacoes_2[0], vetor_olho_objeto_refletido, gltab_luzes[3]),
+          CorLuzPontualEspecular(atenuacoes_2[1], vetor_olho_objeto_refletido, gltab_luzes[4]),
+          CorLuzPontualEspecular(atenuacoes_2[2], vetor_olho_objeto_refletido, gltab_luzes[5]),
+          CorLuzPontualEspecular(atenuacoes_2[3], vetor_olho_objeto_refletido, gltab_luzes[6]));
+
+      // Usa clamp porque especularidade pode aumentar para acima de 1.
       cor_final = clamp(cor_final + cor_luzes_especulares_1 * uns + cor_luzes_especulares_2 * uns, 0.0, 1.0);
     }
   }
