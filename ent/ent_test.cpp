f7c529df1ffe0798ce4c101f050d8309e2a3bd3e
Matheus Ribeiro <mfribeiro+planilha@gmail.com>
2020-03-26 13:27:57 -0300

    fix de ent_test

--- ent/ent_test.cpp
+++ ent/ent_test.cpp
@@ -1,33 +1,37 @@
 #include <memory>
 #include "ent/entidade.h"
 #include "ent/entidade.pb.h"
+#include "ent/tabelas.h"
 #include "gtest/gtest.h"
 
 namespace ent {
 
-// Teste basico gera dados.
+namespace {
+Tabelas g_tabelas(nullptr);
+}  // namespace
+
 TEST(TesteEntidade, TesteAtualizaAcao) {
-  std::unique_ptr<Entidade> e(NovaEntidade(EntidadeProto(), nullptr, nullptr, nullptr));
+  std::unique_ptr<Entidade> e(NovaEntidadeParaTestes(EntidadeProto(), g_tabelas));
   e->AdicionaAcaoExecutada("teste1");
-  EXPECT_EQ("teste1", e->AcaoExecutada(0));
+  EXPECT_EQ("teste1", e->TipoAcaoExecutada(0, {}));
   e->AdicionaAcaoExecutada("teste2");
-  EXPECT_EQ("teste2", e->AcaoExecutada(0));
-  EXPECT_EQ("teste1", e->AcaoExecutada(1));
-  EXPECT_EQ("", e->AcaoExecutada(2));
+  EXPECT_EQ("teste2", e->TipoAcaoExecutada(0, {}));
+  EXPECT_EQ("teste1", e->TipoAcaoExecutada(1, {}));
+  EXPECT_EQ("", e->TipoAcaoExecutada(2, {}));
   e->AdicionaAcaoExecutada("teste1");
-  EXPECT_EQ("teste1", e->AcaoExecutada(0));
-  EXPECT_EQ("teste2", e->AcaoExecutada(1));
-  EXPECT_EQ("", e->AcaoExecutada(2));
+  EXPECT_EQ("teste1", e->TipoAcaoExecutada(0, {}));
+  EXPECT_EQ("teste2", e->TipoAcaoExecutada(1, {}));
+  EXPECT_EQ("", e->TipoAcaoExecutada(2, {}));
   e->AdicionaAcaoExecutada("teste3");
-  EXPECT_EQ("teste3", e->AcaoExecutada(0));
-  EXPECT_EQ("teste1", e->AcaoExecutada(1));
-  EXPECT_EQ("teste2", e->AcaoExecutada(2));
-  EXPECT_EQ("", e->AcaoExecutada(3));
+  EXPECT_EQ("teste3", e->TipoAcaoExecutada(0, {}));
+  EXPECT_EQ("teste1", e->TipoAcaoExecutada(1, {}));
+  EXPECT_EQ("teste2", e->TipoAcaoExecutada(2, {}));
+  EXPECT_EQ("", e->TipoAcaoExecutada(3, {}));
   e->AdicionaAcaoExecutada("teste4");
-  EXPECT_EQ("teste4", e->AcaoExecutada(0));
-  EXPECT_EQ("teste3", e->AcaoExecutada(1));
-  EXPECT_EQ("teste1", e->AcaoExecutada(2));
-  EXPECT_EQ("", e->AcaoExecutada(3));
+  EXPECT_EQ("teste4", e->TipoAcaoExecutada(0, {}));
+  EXPECT_EQ("teste3", e->TipoAcaoExecutada(1, {}));
+  EXPECT_EQ("teste1", e->TipoAcaoExecutada(2, {}));
+  EXPECT_EQ("", e->TipoAcaoExecutada(3, {}));
 }
 
 }  // namespace ent.
