2cc0748ed1c66d9544a8fc6c4c85c3c0e6c3095a
Matheus Ribeiro <mfribeiro+planilha@gmail.com>
2020-04-15 20:03:41 -0300

    versao 4.2.0

--- ifg/qt/menuprincipal.cpp
+++ ifg/qt/menuprincipal.cpp
@@ -583,7 +583,7 @@ void MenuPrincipal::TrataAcaoItem(QAction* acao) {
     QMessageBox::about(
         qobject_cast<QWidget*>(parent()),
         tr("Sobre o tabuleiro virtual"),
-        tr("Tabuleiro virtual versão 4.1.2\n"
+        tr("Tabuleiro virtual versão 4.2.0\n"
            "Bibliotecas: QT, OpenGL, Protobuf, Boost\n"
            "Ícones: origem http://www.flaticon.com/\n"
            "- Designed by Freepik\n"
