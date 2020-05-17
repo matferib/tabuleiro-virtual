fc6ea22f60f239c181363fb5db2ccd4b2276f51a
Matheus Ribeiro <mfribeiro@gmail.com>
2020-05-17 19:00:49 -0300

    vlog no som

--- som/som_qt.cpp
+++ som/som_qt.cpp
@@ -9,7 +9,7 @@ using google::protobuf::StringPrintf;
 namespace som {
 
 void Toca(const std::string& nome) {
-  LOG(INFO) << "tocando: " << StringPrintf("%s/%s", arq::Diretorio(arq::TIPO_SOM).c_str(), nome.c_str());
+  VLOG(1) << "tocando: " << StringPrintf("%s/%s", arq::Diretorio(arq::TIPO_SOM).c_str(), nome.c_str());
   QSound::play(QString::fromStdString(StringPrintf("%s/%s", arq::Diretorio(arq::TIPO_SOM).c_str(), nome.c_str())));
 }
 
