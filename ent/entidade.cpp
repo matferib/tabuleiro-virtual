537edd6357a86495e02146956ca19ff16b91dfc9
Matheus Ribeiro <mfribeiro@gmail.com>
2020-05-19 23:24:56 -0300

    fix de short sword

--- ent/entidade.cpp
+++ ent/entidade.cpp
@@ -2099,7 +2099,7 @@ void Entidade::IniciaGl(ntf::CentralNotificacoes* central) {
   }
   // Vbos de armas.
   std::vector<std::string> dados_vbo = {
-    "kama", "quarterstaff", "sword", "bow", "club", "shield", "hammer", "flail", "crossbow", "axe", "shield", "mace", "morning_star", "spear"
+    "kama", "quarterstaff", "sword", "short_sword", "bow", "club", "shield", "hammer", "flail", "crossbow", "axe", "shield", "mace", "morning_star", "spear"
   };
   for (const auto& id : dados_vbo) {
     std::unique_ptr<ntf::Notificacao> n(ntf::NovaNotificacao(ntf::TN_CARREGAR_MODELO_3D));
