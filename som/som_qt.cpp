73c7229be54b739f39f6aad958180466f62af87d
Matheus Ribeiro <mfribeiro@gmail.com>
2020-06-12 03:29:31 -0300

    implementacao com sound fx

--- som/som_qt.cpp
+++ som/som_qt.cpp
@@ -1,4 +1,5 @@
 #include <QFileInfo>
+#include <QSoundEffect>
 #include <QtMultimedia/QMediaPlayer>
 #include <QThread>
 #include <list>
@@ -14,25 +15,32 @@ namespace som {
 namespace {
 
 std::unique_ptr<QMediaPlayer> g_media_player;
+std::unique_ptr<QSoundEffect> g_fx;
 
 }  // namespace
 
 void Inicia() {
   g_media_player = std::make_unique<QMediaPlayer>();
+  g_fx = std::make_unique<QSoundEffect>();
 }
 
 void Finaliza() {
   g_media_player.reset();
+  g_fx.reset();
 }
 
 void Toca(const std::string& nome) {
-  QString qs = QFileInfo(QString::fromStdString(s)).absoluteFilePath();
-  g_media_player->setMedia(QUrl::fromLocalFile(qs));
-  g_media_player->setVolume(100);
-  g_media_player->play();
-  VLOG(1)
-      << "tentando tocar " << s << " vol: " << g_media_player->volume() << ", error: " << static_cast<int>(g_media_player->error())
-      << ", availability: " << static_cast<int>(g_media_player->availability()) << ", media status: " << (int)g_media_player->mediaStatus();
+  QString qs = QFileInfo(QString::fromStdString(StringPrintf("%s/%s", arq::Diretorio(arq::TIPO_SOM).c_str(), nome.c_str()))).absoluteFilePath();
+  g_fx->setSource(QUrl::fromLocalFile(qs));
+  g_fx->setLoopCount(1);
+  //LOG(INFO) << "status: "  << fx.status();
+  g_fx->play();
+  //g_media_player->setMedia(QUrl::fromLocalFile(qs));
+  //g_media_player->setVolume(100);
+  //g_media_player->play();
+  //VLOG(1)
+  //    << "tentando tocar " << qs.toStdString() << " vol: " << g_media_player->volume() << ", error: " << static_cast<int>(g_media_player->error())
+  //    << ", availability: " << static_cast<int>(g_media_player->availability()) << ", media status: " << (int)g_media_player->mediaStatus();
 }
 
 }  // namespace som
