dbc02da73612ac0814d8eee5d866f7dcc6aad150
Matheus Ribeiro <mfribeiro@gmail.com>
2020-06-23 21:03:13 -0300

    versao 4.11.0

--- ifg/qt/menuprincipal.cpp
+++ ifg/qt/menuprincipal.cpp
@@ -604,7 +604,7 @@ void MenuPrincipal::TrataAcaoItem(QAction* acao) {
     QMessageBox::about(
         qobject_cast<QWidget*>(parent()),
         tr("Sobre o tabuleiro virtual"),
-        tr("Tabuleiro virtual versão 4.10.0\n"
+        tr("Tabuleiro virtual versão 4.11.0\n"
            "Bibliotecas: QT, OpenGL, Protobuf, Boost\n"
            "Ícones: origem http://www.flaticon.com/\n"
            "- Designed by Freepik\n"
