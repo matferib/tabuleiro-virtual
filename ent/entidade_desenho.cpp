d233ae0f284988645bca42c9a736853a6f81b8e5
Matheus Ribeiro <mfribeiro@gmail.com>
2020-07-28 12:23:55 -0300

    comentarior static na funcao de Desenha

--- ent/entidade_desenho.cpp
+++ ent/entidade_desenho.cpp
@@ -128,6 +128,7 @@ void Entidade::DesenhaObjetoProto(const EntidadeProto& proto, const VariaveisDer
   }
 }
 
+// static
 void Entidade::DesenhaObjetoEntidadeProtoComMatrizes(
     const EntidadeProto& proto, const VariaveisDerivadas& vd, ParametrosDesenho* pd,
     const Matrix4& modelagem, const Matrix4& tijolo_base, const Matrix4& tijolo_tela, const Matrix4& tela_textura, const Matrix4& deslocamento_textura) {
