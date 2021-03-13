86719c7f62fc8342c3b55f0b5e72192d759ef075
Matheus Ribeiro <mfribeiro@gmail.com>
2021-03-13 14:23:32 -0300

    correcao de script

--- tabvirt.sh
+++ tabvirt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #export LD_LIBRARY_PATH=/home/matheus/Projetos/libs/Qt/5.11.1/gcc_64/lib
-export LD_LIBRARY_PATH=/home/matheus/Projetos/libs/Qt/5.11.1/gcc_64/lib:/home/matheus/Projetos/libs/boost_1_67_0/stage/lib/
+export LD_LIBRARY_PATH=/home/matheus/Documentos/Projetos/libs/Qt/5.15.0/gcc_64/lib:/home/matheus/Documentos/Projetos/libs/boost_1_67_0/stage/lib/:/home/matheus/Documentos/Projetos/libs/tbb/build/linux_intel64_gcc_cc9.3.0_libc2.31_kernel5.4.0_release/
 #export QT_DEBUG_PLUGINS=1
 
 if [ "$1" == "debug" ]; then
