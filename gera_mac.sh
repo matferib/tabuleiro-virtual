0675c0c0683c005875114b26f986f0102c7b1acd
Matheus Ribeiro <mribeiro@google.com>
2024-11-16 19:38:17 -0300

    mais updates qt6

--- gera_mac.sh
+++ gera_mac.sh
@@ -29,7 +29,7 @@ echo "Escrevendo em ${BUNDLEDIR}"
 RESOURCESDIR=${BUNDLEDIR}/Contents/Resources
 XDIR=${BUNDLEDIR}/Contents/MacOS
 FRAMEWORKSDIR=${BUNDLEDIR}/Contents/Frameworks
-MACDEPLOYQT=/opt/homebrew/Cellar/qt@5/5.15.15/bin/macdeployqt
+MACDEPLOYQT=/opt/homebrew/Cellar/qt/6.7.2_2/bin/macdeployqt6
 
 make apple && \
 mkdir -p ${XDIR} ${RESOURCESDIR}/tabuleiros_salvos ${RESOURCESDIR}/dados ${RESOURCESDIR}/shaders ${RESOURCESDIR}/texturas ${RESOURCESDIR}/modelos3d ${RESOURCESDIR}/sons && \
@@ -43,10 +43,6 @@ cp -f modelos3d/*.binproto ${RESOURCESDIR}/modelos3d && \
 cp -f sons/*.wav ${RESOURCESDIR}/sons && \
 cp -f tabuleiros_salvos/*.binproto ${RESOURCESDIR}/tabuleiros_salvos && \
 chmod -R a+r ${BUNDLEDIR}/ &&  \
-for i in libprotobuf.9.dylib libboost_system.dylib libboost_timer.dylib libboost_filesystem.dylib; do
-  set -x
-  echo skipping install_name_tool -change /usr/local/lib/${i} @executable_path/../Frameworks/${i} ${XDIR}/tabvirt;
-  set +x
-done && \
 ${MACDEPLOYQT} macqt5bundle -always-overwrite && \
+codesign -s - --deep macqt5bundle/Contents/MacOS/tabvirt && \
 pkgbuild --identifier com.matferib.TabuleiroVirtual --version ${VERSAO} --install-location=/Applications/TabuleiroVirtual.app --root ./${BUNDLEDIR} TabuleiroVirtual-${VERSAO}.pkg
