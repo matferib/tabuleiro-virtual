fbc36a7a5ee6782df15b59ee849364e498d74be9
Matheus Ribeiro <mribeiro@google.com>
2025-12-25 13:23:09 -0300

    som dummy

---
+++ som/som_dummy.cpp
@@ -0,0 +1,14 @@
+#include "som/som.h"
+
+namespace som {
+
+void Inicia(const ent::OpcoesProto& opcoes) {
+}
+
+void Finaliza() {
+}
+
+void Toca(const std::string& nome) {
+}
+
+}  // namespace som
