98c3d175486609c84e4659367fe4e2bca58c9cfd
Matheus Ribeiro <mribeiro@google.com>
2026-09-16 13:09:30 -0300

    comentarios

--- som/som.h
+++ som/som.h
@@ -7,7 +7,9 @@
 namespace som {
 
 void Inicia(const ent::OpcoesProto& opcoes);
+// Apenas arquivos wav permitidos.
 void Toca(const std::string& nome);
+// Permite wav e ogg.
 void TocaSomFundo(const std::string& nome);
 void ParaSomFundo(const std::string& nome);
 void Finaliza();
